e671434fbb68b3173b3b758c02e4676b56db0b69
Rasika Gayan <rasikagayangunarathna@gmail.com>
2015-12-03 10:33:48 -0800

    init commit

---
+++ app/src/main/java/com/rasikagayan/android/criminalintent/Crime.java
@@ -0,0 +1,14 @@
+package com.rasikagayan.android.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by Rasika Gayan on 12/3/2015.
+ */
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+
+    
+}
---
+++ app/src/main/java/com/rasikagayan/android/criminalintent/CrimeActivity.java
@@ -0,0 +1,15 @@
+package com.rasikagayan.android.criminalintent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CrimeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+    }
+
+
+}
