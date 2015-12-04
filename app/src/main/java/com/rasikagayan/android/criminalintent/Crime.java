47e0b7c99b4b8e7ad538c410a4a505d4dee4491a
Rasika Gayan <rasikagayangunarathna@gmail.com>
2015-12-03 17:28:33 -0800

    added fragment to application and host the crime fragment

--- app/src/main/java/com/rasikagayan/android/criminalintent/Crime.java
+++ app/src/main/java/com/rasikagayan/android/criminalintent/Crime.java
@@ -10,5 +10,19 @@ public class Crime {
     private UUID mId;
     private String mTitle;
 
-    
+    public Crime(){
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getmId() {
+        return mId;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
 }
